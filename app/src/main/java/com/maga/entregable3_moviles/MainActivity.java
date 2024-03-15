be98b08c13f666577a47f2405da013b92e295e6a
MAGA1314 <garciaagudelomiguelangel@gmail.com>
2024-03-14 19:12:16 -0500

    imagenes

--- app/src/main/java/com/maga/entregable3_moviles/MainActivity.java
+++ app/src/main/java/com/maga/entregable3_moviles/MainActivity.java
@@ -2,13 +2,50 @@ package com.maga.entregable3_moviles;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView ListarCorreos;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        String[] nombres = {
+                "Luis",
+                "Juan",
+                "Pedro"
+        };
+        String[] telefonos = {
+                "123",
+                "345",
+                "678"
+        };
+        int[] fotoperfil = {
+                R.drawable.img2,
+                R.drawable.img3,
+                R.drawable.img4
+        };
+        ListAdapter personas = new ListAdapter(MainActivity.this, nombres, telefonos, fotoperfil);
+        ListarCorreos = (ListView) findViewById(R.id.listausuarios);
+        ListarCorreos.setAdapter(personas);
+
+        ListarCorreos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> lista, View vista, int posicion, long id) {
+                Intent enviarInfo = new Intent(MainActivity.this, SegundaPantalla.class)
+                        .putExtra("nombre", nombres[posicion])
+                        .putExtra("telefono", telefonos[posicion])
+                        .putExtra("imagen", fotoperfil[posicion]);
+
+                startActivity(enviarInfo);
+            }
+        });
     }
 }
---
+++ app/src/main/java/com/maga/entregable3_moviles/SegundaPantalla.java
@@ -0,0 +1,4 @@
+package com.maga.entregable3_moviles;
+
+public class SegundaPantalla {
+}
