3591d68c955fae2366e5a9d55281e5c52a1d8c64
MAGA1314 <garciaagudelomiguelangel@gmail.com>
2024-03-15 19:59:14 -0500

    pasar la informacion a la segunda pantalla listo

--- app/src/main/java/com/maga/entregable3_moviles/SegundaPantalla.java
+++ app/src/main/java/com/maga/entregable3_moviles/SegundaPantalla.java
@@ -4,8 +4,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Color;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -57,7 +55,9 @@ public class SegundaPantalla extends AppCompatActivity {
                 // Por ejemplo, cambiar el color de fondo de un elemento de la vista
                 textoNombre.setBackgroundColor(Color.GRAY);
             }
+
         }
     }
+
 }
 
